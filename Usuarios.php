c203e417d8114a265923821e6a342e7520322207
Camilo Hernandez <ch231322@gmail.com>
2025-04-07 18:43:35 -0500

    funcion para edad

--- Usuarios.php
+++ Usuarios.php
@@ -1,15 +1,18 @@
 <?php
 include_once 'database.php';
 
-class Usuario {
+class Usuario
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $conexion = new Database();
         $this->db = $conexion->conectar();
     }
 
-    public function crear($nombre, $apellido, $fecha, $telefono, $correo, $direccion) {
+    public function crear($nombre, $apellido, $fecha, $telefono, $correo, $direccion)
+    {
         $sql = "INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, telefono, correo, direccion)
                 VALUES ('$nombre', '$apellido', '$fecha', '$telefono', '$correo', '$direccion')";
 
@@ -20,7 +23,8 @@ class Usuario {
         }
     }
 
-    public function listar() {
+    public function listar()
+    {
         $resultado = $this->db->query("SELECT * FROM usuarios");
 
         while ($fila = $resultado->fetch_assoc()) {
@@ -29,8 +33,9 @@ class Usuario {
             echo "ID: {$fila['id']} | $nombre | Edad: $edad | Tel: {$fila['telefono']}\n";
         }
     }
-    
-    public function actualizar($id, $telefono, $correo, $direccion) {
+
+    public function actualizar($id, $telefono, $correo, $direccion)
+    {
         $sql = "UPDATE usuarios SET telefono='$telefono', correo='$correo', direccion='$direccion' WHERE id=$id";
 
         if ($this->db->query($sql)) {
@@ -39,14 +44,25 @@ class Usuario {
             echo "Error al actualizar.\n";
         }
     }
-    public function eliminar($id) {
+
+    public function eliminar($id)
+    {
         $sql = "DELETE FROM usuarios WHERE id=$id";
 
         if ($this->db->query($sql)) {
             echo "Usuario eliminado.\n";
         } else {
             echo "Error al eliminar.\n";
-        }
-    }
+        }
+    }
+
+    public  function edad($fecha)
+    {
+        $nacimiento = new DateTime($fecha);
+        $hoy = new DateTime();
+        $edad = $hoy->diff($nacimiento)->y;
+        return $edad;
+    }
 }
+
 ?>
